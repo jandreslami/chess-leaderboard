ab848456f714c56d3828d7a1b959a2dd005fec07
jandreslami <115507664+jandreslami@users.noreply.github.com>
2023-04-18 20:10:54 -0300

    Creada funcionalidad básica
    La tabla se muestra correctamente

--- src/index.js
+++ src/index.js
@@ -1,10 +1,67 @@
-console.log("hola")
+let url = "https://api.chess.com/pub/leaderboards";
 
-let url =
-  "https://api.chess.com/pub/leaderboards";
-//devuelve 20 primeros jugadores
+let TipoLeaderboard = "live_blitz";
+
+//  Tipos ranking: live_blitz,
+//   live_bullet,
+//   live_rapid,
+
+function crearTablaLeaderboard(objetoJugador) {
+  objetoJugador.forEach((elemento) => {
+    let fila = document.createElement("tr");
+
+    let ranking = document.createElement("th");
+    ranking.textContent(elemento);
+
+    fila.appendChild(ranking);
+  });
+  document.appendChild();
+}
 
 fetch(url)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error(error));
+  .then((respuesta) => respuesta.json())
+  .then((datos) => {
+    const cuerpoTabla = document.querySelector("#cuerpo-tabla");
+
+    datos[TipoLeaderboard].forEach((elemento) => {
+      let fila = document.createElement("tr");
+
+      let ranking = document.createElement("th");
+      ranking.textContent = elemento.rank;
+   
+
+      let nombre = document.createElement("td");
+      nombre.textContent = elemento.name;
+
+      let rating = document.createElement("td");
+      rating.textContent = elemento.score;
+
+      let numeroPartidasGanadas = Number(elemento.win_count);
+      let numeroPartidasPerdidas = Number(elemento.loss_count);
+      let numeroPartidasTablas = Number(elemento.draw_count);
+      let numeroPartidasTotales =
+        numeroPartidasGanadas + numeroPartidasPerdidas + numeroPartidasTablas;
+
+      let totalPartidas = document.createElement("td");
+      totalPartidas.textContent = numeroPartidasTotales;
+
+      let porcentajeGanadas = document.createElement("td");
+      porcentajeGanadas.textContent = `${(
+        (numeroPartidasGanadas / numeroPartidasTotales) *
+        100
+      ).toFixed(2)}%`;
+
+      fila.appendChild(ranking);
+      fila.appendChild(nombre);
+      fila.appendChild(rating);
+      fila.appendChild(totalPartidas);
+      fila.appendChild(porcentajeGanadas);
+
+      cuerpoTabla.appendChild(fila);
+    });
+
+    console.log(datos[TipoLeaderboard]);
+  })
+
+
+  .catch((error) => console.error(error));
