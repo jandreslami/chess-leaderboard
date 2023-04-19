dfcbe44093e6f89d2cbaf108d65d51e2d08e3d5c
jandreslami <115507664+jandreslami@users.noreply.github.com>
2023-04-19 13:44:47 -0300

    Renombra variables que son nodos HTML con $nodo
    La intención es que mejore la legibilidad

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ let url = "https://api.chess.com/pub/leaderboards";
 
 let TipoLeaderboard = "live_blitz";
 
-//  Tipos ranking: live_blitz,
+//  Tipos $ranking: live_blitz,
 //   live_bullet,
 //   live_rapid,
 
@@ -12,25 +12,25 @@ fetch(url)
     const cuerpoTabla = document.querySelector("#cuerpo-tabla");
 
     datos[TipoLeaderboard].forEach((elemento) => {
-      let fila = document.createElement("tr");
+      let $fila = document.createElement("tr");
 
-      let ranking = document.createElement("th");
-      ranking.textContent = elemento.rank;
-      ranking.style.width = "16px";
+      let $ranking = document.createElement("th");
+      $ranking.textContent = elemento.rank;
+      $ranking.style.width = "16px";
       
-      let nombre = document.createElement("td");
-      nombre.textContent = elemento.name;
+      let $nombre = document.createElement("td");
+      $nombre.textContent = elemento.name;
 
-      let contenedorBandera = document.createElement("td");
-      contenedorBandera.style.width = "16px";
+      let $contenedor$Bandera = document.createElement("td");
+      $contenedor$Bandera.style.width = "16px";
 
       let paisJugador = elemento.country.slice(34).toLowerCase();
-      let bandera = document.createElement("span");
-      bandera.classList.add(`fi`, `fi-${paisJugador}`);
-      contenedorBandera.appendChild(bandera);
+      let $bandera = document.createElement("span");
+      $bandera.classList.add(`fi`, `fi-${paisJugador}`);
+      $contenedor$Bandera.appendChild($bandera);
 
-      let rating = document.createElement("td");
-      rating.textContent = elemento.score;
+      let $rating = document.createElement("td");
+      $rating.textContent = elemento.score;
 
       let numeroPartidasGanadas = Number(elemento.win_count);
       let numeroPartidasPerdidas = Number(elemento.loss_count);
@@ -38,23 +38,23 @@ fetch(url)
       let numeroPartidasTotales =
         numeroPartidasGanadas + numeroPartidasPerdidas + numeroPartidasTablas;
 
-      let totalPartidas = document.createElement("td");
-      totalPartidas.textContent = numeroPartidasTotales;
+      let $totalPartidas = document.createElement("td");
+      $totalPartidas.textContent = numeroPartidasTotales;
 
-      let porcentajeGanadas = document.createElement("td");
-      porcentajeGanadas.textContent = `${(
+      let $porcentajeGanadas = document.createElement("td");
+      $porcentajeGanadas.textContent = `${(
         (numeroPartidasGanadas / numeroPartidasTotales) *
         100
       ).toFixed(2)}%`;
 
-      fila.appendChild(ranking);
-      fila.appendChild(contenedorBandera);
-      fila.appendChild(nombre);
-      fila.appendChild(rating);
-      fila.appendChild(totalPartidas);
-      fila.appendChild(porcentajeGanadas);
+      $fila.appendChild($ranking);
+      $fila.appendChild($contenedor$Bandera);
+      $fila.appendChild($nombre);
+      $fila.appendChild($rating);
+      $fila.appendChild($totalPartidas);
+      $fila.appendChild($porcentajeGanadas);
 
-      cuerpoTabla.appendChild(fila);
+      cuerpoTabla.appendChild($fila);
     });
 
     console.log(datos[TipoLeaderboard]);
